93810a82c4bc2dd9d866530f7c7f303073b6619e
Spenser Bushey <spenser@spenserbushey.com>
2025-06-13 20:49:00 +0200

    Remove download limit

--- src/env.ts
+++ src/env.ts
@@ -2,12 +2,13 @@ import { z } from "zod/v4";
 import { parseEnv } from "@Weldawadyathink/zod-env"; //TODO: Switch to @keawade/zod-env once PR is merged
 
 export const env = parseEnv(z.object({
-  DATABASE_URL: z.string().url(),
+  DATABASE_URL: z.url(),
   REPLICATE_API_TOKEN: z.string(),
   AWS_ACCESS_KEY_ID: z.string(),
   AWS_SECRET_ACCESS_KEY: z.string(),
   AWS_REGION: z.string(),
-  AWS_ENDPOINT_URL_S3: z.string().url(),
+  AWS_ENDPOINT_URL_S3: z.url(),
   AWS_S3_BUCKET: z.string(),
-  NEW_POSTS_TO_DOWNLOAD: z.coerce.number().int().min(1),
+  REDDIT_SORT_TIME: z.string(),
+  REDDIT_SORT: z.string(),
 }));
